28ec8bbad5461f84d1171a4980b5d4454719be9e
Pedro José Fernández Franco <uo224775@uniovi.es>
2021-01-29 11:14:14 +0100

    Aumentada la velocidad de probing

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -910,13 +910,13 @@
  *
  * Specify a Probe position as { X, Y, Z }
  */
-#define NOZZLE_TO_PROBE_OFFSET { -42.53, -10, -0.76 }
+#define NOZZLE_TO_PROBE_OFFSET { -42.53, -10, -0.75 }
 
 // Certain types of probes need to stay away from edges
 #define MIN_PROBE_EDGE 10
 
 // X and Y axis travel speed (mm/m) between probes
-#define XY_PROBE_SPEED 8000
+#define XY_PROBE_SPEED 10000
 
 // Feedrate (mm/m) for the first approach when double-probing (MULTIPLE_PROBING == 2)
 #define Z_PROBE_SPEED_FAST HOMING_FEEDRATE_Z
@@ -1199,7 +1199,7 @@
 #if EITHER(AUTO_BED_LEVELING_LINEAR, AUTO_BED_LEVELING_BILINEAR)
 
   // Set the number of grid points per dimension.
-  #define GRID_MAX_POINTS_X 3
+  #define GRID_MAX_POINTS_X 2
   #define GRID_MAX_POINTS_Y GRID_MAX_POINTS_X
 
   // Probe along the Y axis, advancing X after each column
